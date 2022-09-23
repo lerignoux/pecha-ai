a5c99c6af76d69d4c0d6bcc1138af6df8634c259
Laurent Erignoux <lerignoux@gmail.com>
2022-09-23 16:30:00 +0800

    Updating pecha-ai with a basic working image and pptp generation

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import logging
 
 from flask import Flask, render_template
 
+from presentation_generation.pptx import Pptx
+from image_generation.stability import Stability
+
+Ais = {
+    'stability': Stability
+}
 
 log = logging.getLogger('peka-ai')
 
@@ -9,6 +15,11 @@ parser = argparse.ArgumentParser(description='Show transversal xml properties.')
 parser.add_argument('--debug', '-d', dest='debug',
                     action='store_true',
                     help='Debug mode')
+
+
+def get_config(ai):
+    with open('config.json', 'r') as f:
+        return json.load(f).get(ai)
 
 
 @app.before_first_request
@@ -27,6 +38,19 @@ def initialize():
 @app.route('/', methods=['GET'])
 def root():
     return render_template('root.html')
+
+
+def generate_pecha(title, inputs, ai='Stability'):
+    config = get_config(ai)
+    ai = Ais[ai](title, config)
+
+    images = []
+    for input in inputs:
+        image = ai.generate(input, title)
+        images.append((title, image))
+
+    filename = f"{title}.pptx"
+    Pptx(title).generate(images, filename)
 
 
 if __name__ == "__main__":
